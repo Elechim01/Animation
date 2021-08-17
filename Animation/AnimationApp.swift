7b1dc28881465fac1ac3c445f7565ccb8dd8f89b
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-08-17 22:42:15 +0200

    Initial Commit

---
+++ Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Michele Manniello on 17/08/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
